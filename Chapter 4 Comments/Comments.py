3f88d8d241a7e7c507da9c1359fe9e750af762f9
apurvanimkar <nimkarapurva2000@gmail.com>
2023-03-02 22:39:51 +0530

    commint

---
+++ Chapter 4 Comments/Comments.py
@@ -0,0 +1,17 @@
+print("-------singleline Comments-------")
+#This is a comment.
+print("Hello, World!")
+
+
+print("-------Multiline Comments-------")
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!") 
